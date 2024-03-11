6c843bb0705ecae2cd5b632770c0568bfea6b4a5
Akhil Ram Shankar <ramshankar.akhil@gmail.com>
2024-03-10 17:03:34 -0700

    stop command to remote_controller

--- remote_controller.py
+++ remote_controller.py
@@ -22,19 +22,27 @@ dribble = 0x01
 kick = 0x02
 chip = 0x03
 
-while True:
-    message = str(b'11110abc0abc0abc0abc')
-        
-    sendToEmbedded(wheelAll)
+try:
+    while True:
+        message = str(b'11110abc0abc0abc0abc')
+            
+        sendToEmbedded(wheelAll)
 
-    actual_b = readFromEmbedded()
-    
-    t=0
-    if (len(sys.argv) > 1 and sys.argv[1] == "-a"):
-        visuals = Plotter()
-        expectedRpmArray = hexToRpmArray(4, str(message))
-        actualRpmArray = hexToRpmArray(4, actual_b) 
-        print(f"Expected: {expectedRpmArray}")
-        print(f"Actual: {actualRpmArray}")
-        visuals.update_plot(t, expectedRpmArray, actualRpmArray)
-        t += 1	
+        actual_b = readFromEmbedded()
+        
+        t=0
+        if (len(sys.argv) > 1 and sys.argv[1] == "-a"):
+            visuals = Plotter()
+            expectedRpmArray = hexToRpmArray(4, str(message))
+            actualRpmArray = hexToRpmArray(4, actual_b) 
+            print(f"Expected: {expectedRpmArray}")
+            print(f"Actual: {actualRpmArray}")
+            visuals.update_plot(t, expectedRpmArray, actualRpmArray)
+            t += 1	
+
+		
+except KeyboardInterrupt:
+    print("\nProgram terminated.")
+
+finally:
+    stopAll()
