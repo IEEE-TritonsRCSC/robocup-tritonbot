e2c1228e705ae0209a7c0bbcbedceec5996cf2cd
Akhil-py <aramshankar@ucsd.edu>
2024-04-14 13:36:52 -0700

    gradual increase in dribbler speed

--- interface/dribbler.py
+++ interface/dribbler.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+
 def setup_dribbler_pwm():
         # enable pwm on gpio pin 18.
         # will say "I/O error" after the first call. not an issue
@@ -15,19 +16,22 @@ def setup_dribbler_pwm():
         os.system("echo 1 > /sys/class/pwm/pwmchip2/pwm2/enable")
 
 def dribble_on():
-        os.system("echo 1100000 > /sys/class/pwm/pwmchip2/pwm2/duty_cycle")
+	curr = int(os.popen("cat /sys/class/pwm/pwmchip2/pwm2/duty_cycle").read())
+	if curr < 1100000:
+		new = curr + 50
+		print(f"Increasing dribbler pulsewidth: {new}")
+		os.system("echo " + str(new) + " > /sys/class/pwm/pwmchip2/pwm2/duty_cycle")
+	else:
+		print("Dribbler at pulsewidth: " + str(curr))
+		os.system("echo 1100000 > /sys/class/pwm/pwmchip2/pwm2/duty_cycle")
 
 def dribble_off():
-        os.system("echo 1000000 > /sys/class/pwm/pwmchip2/pwm2/duty_cycle")
-
-print("starting")
-setup_dribbler_pwm()
-time.sleep(1)
-print("on")
-dribble_on()
-time.sleep(1)
-print("off")
-dribble_off()
-time.sleep(1)
-print("done")
+	curr = int(os.popen("cat /sys/class/pwm/pwmchip2/pwm2/duty_cycle").read())
+	if curr > 1000000:
+		new = curr - 50
+		print(f"Decreasing dribbler pulsewidth: {new}")
+		os.system("echo " + str(new) + " > /sys/class/pwm/pwmchip2/pwm2/duty_cycle")
+	else:
+		print("Dribbler at min pulsewidth: " + str(curr))
+		os.system("echo 1000000 > /sys/class/pwm/pwmchip2/pwm2/duty_cycle")
 
